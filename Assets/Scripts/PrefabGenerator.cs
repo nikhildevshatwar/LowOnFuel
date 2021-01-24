2a534d5ca1a9fcbade0834a0eacf4e23d91aea60
Nikhil <nikhildevshatwar@gmail.com>
2021-01-24 23:55:08 +0530

    Spawn the planets based on player position
    Also add a secondary camer, yet to get it working
    
    Signed-off-by: Nikhil <nikhildevshatwar@gmail.com>

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -11,23 +11,19 @@ public class PrefabGenerator : MonoBehaviour
 
     private List<GameObject> clones = new List<GameObject>();
     private Vector2 vector;
-    private int count;
+    private float lastCloneHeight = 0;
 
-    void Start()
-    {
-        count = 0;
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        count++;
-        if (count % interval == 0) {
+        if (PlayerControl.height > lastCloneHeight + spawnRange) {
+
             float randX = Random.Range(-spawnRange, spawnRange);
-            float randY = Random.Range(-spawnRange, spawnRange);
+            float randY = PlayerControl.height + spawnRange + Random.Range(0, spawnRange);
+
             vector = new Vector2(randX, randY);
             GameObject clone = Instantiate(originalObject, vector, Quaternion.identity) as GameObject;
             clones.Add(clone);
+            lastCloneHeight = randY;
         }
     }
 }
