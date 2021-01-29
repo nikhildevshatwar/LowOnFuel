d6565268bc63276c7d72a56058bc1c4a59849778
Nikhil <nikhildevshatwar@gmail.com>
2021-01-30 02:52:36 +0530

    Gameplay: Tune the parameters of the gameplay
    Use a thrustScale used to control how much of original thrust is applied
    when the space is not pressed.
    
    TODO
    Adjust the parameters as reuired
    
    Signed-off-by: Nikhil <nikhildevshatwar@gmail.com>

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -13,7 +13,6 @@ public class PlayerControl : MonoBehaviour
     public float initialFuel;
     public float fuelSpeed;
     public float turnSpeed;
-    public static int weight;
     public int totalHealth;
 
     public GameObject trailParticle;
@@ -27,22 +26,20 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private GameObject[] thrusters; //0 is main, 1 is left, 2 right thruster
 
-    [SerializeField]
-    private int startWeight=5;
-
     public static float fuelLevel;
     public static float height;
     private Rigidbody2D rb;
 
     private bool shield_active;
     private int health;
+    public float thrustScale = 0.1f;
+    public static int weight = 10;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         fuelLevel = initialFuel;
-        weight = startWeight;
-        setWeight();
         EM = trailParticle.GetComponent<ParticleSystem>().emission;
 
 
@@ -82,19 +79,11 @@ public class PlayerControl : MonoBehaviour
             
             rb.AddRelativeForce(new Vector2(0, mainThrust));
             fuelLevel -= fuelSpeed;
-            rb.drag = 0.5f;  //set flying drag
-            //show main thruster sprite:
             thrusters[0].SetActive(true);
-        }
-        else if (Input.GetKey(KeyCode.Space) && fuelLevel <= 0)
-        { //emergency thrusters, if main thrusters are jettisoned or if out of fuel
-            rb.AddRelativeForce(new Vector2(0, emergencyThrust));
-            rb.drag = 0.5f;
-            thrusters[0].SetActive(false);
         }
         else
         {
-            rb.drag = .7f; //drifting drag
+            rb.AddRelativeForce(new Vector2(0, mainThrust * thrustScale));
             thrusters[0].SetActive(false);
         }
 
@@ -108,45 +97,39 @@ public class PlayerControl : MonoBehaviour
             rb.AddTorque(turnSpeed);
             thrusters[1].SetActive(true);
         }
-        else { thrusters[1].SetActive(false); }
+        else
+        {
+            thrusters[1].SetActive(false);
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddTorque(-turnSpeed);
             thrusters[2].SetActive(true);
         }
-        else { thrusters[2].SetActive(false); }
+        else
+        {
+            thrusters[2].SetActive(false);
+        }
     }
 
     public void DropShielding()
     {
         AudioManager.Instance.Play("drop");
-        if (weight > 0)
-        {
-            weight -= 2;
-        }
-        setWeight();
+        thrustScale += 0.05f;
     }
 
     public void DropWeapon()
     {
         AudioManager.Instance.Play("drop");
         DisableWeapons();
-        if (weight > 0)
-        {
-            weight -= 2;
-        }
-        setWeight();
+        thrustScale += 0.08f;
     }
 
     public void DropRadar()
     {
         AudioManager.Instance.Play("drop");
-        if (weight > 0)
-        {
-            weight -= 2;
-        }
-        setWeight();
+        thrustScale += 0.05f;
     }
 
     void DisableWeapons()
@@ -154,62 +137,10 @@ public class PlayerControl : MonoBehaviour
         //to do 
     }
 
-    public void DropMainThruster()
+    public void DropPayload()
     {
         AudioManager.Instance.Play("drop");
-        mainThrust = 5;
-        if (weight > 0)
-        {
-            weight--;
-        }
-        setWeight();
-
-    }
-
-    public void DropSteeringThrusters()
-    {
-        AudioManager.Instance.Play("drop");
-        if (weight > 0)
-        {
-            weight--;
-        }
-        setWeight();
-        turnSpeed = turnSpeed / 2;
-    }
-
-   
-
-    void setWeight()
-    {
-        if (weight == 1){
-            rb.mass = 1;
-            turnSpeed = 100;
-            fuelSpeed = 1;
-        }
-        if (weight == 2)
-        {
-            rb.mass = 2;
-            turnSpeed = 80;
-            fuelSpeed = 3;
-        }
-        if (weight == 3)
-        {
-            rb.mass = 3;
-            turnSpeed = 60;
-            fuelSpeed = 5;
-        }
-        if (weight == 4)
-        {
-            rb.mass = 4;
-            turnSpeed = 40;
-            fuelSpeed = 7;
-        }
-        if (weight == 5)
-        {
-            rb.mass = 5;
-            turnSpeed = 35;
-            fuelSpeed = 9;
-        }
+        thrustScale += 0.03f;
     }
 
     void EngineSound()
